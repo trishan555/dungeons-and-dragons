b800b027e2bafa37967d548b679bb653c47552b8
TrishLeaf <trishanp96@gmail.com>
2022-09-16 19:43:16 +0530

    styled done - Spells

--- src/Components/Spells.jsx
+++ src/Components/Spells.jsx
@@ -18,18 +18,62 @@ const Spells = () => {
     }
 
     return (
-        <div>
+        <Wrapper>
             <h3>Popular Picks</h3>
 
             {spellData.map((spell) => {
                 return (
-                    <div>
+                    <Card>
                         <p>{spell.name}</p>
-                    </div>
+                        <Gradiant />
+                    </Card>
                 )
             })}
-        </div>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    margin: 4rem 0rem;
+`
+
+const Card = styled.div`
+    min-height: 20rem;
+    border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+
+    img {
+        border-radius: 2rem;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        object-fit: cover;
+        /* height: auto; */
+    }
+
+    p {
+        z-index: 10;
+        color: white;
+        /* display: flex;
+        justify-content: center;
+        align-items: center; */
+        position: absolute;
+        font-size: 1rem;
+        font-weight: 600;
+        text-align: center;
+        left: 50%;
+        bottom: 10%;
+        transform: translate(-50%, 0);
+    }
+`
+
+const Gradiant = styled.div`
+    z-index: 3;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`
 
 export default Spells
