44f09c8fdfbd7d5c62012ad6eb8ef3ca4a93eb8f
TrishLeaf <trishanp96@gmail.com>
2022-09-16 22:40:47 +0530

    show detailed each spell

--- src/Components/Spell.jsx
+++ src/Components/Spell.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
 
 const Spell = () => {
     const [details, setDetails] = useState({})
+    const [activeTab, setActiveTab] = useState('instructions')
     const params = useParams()
 
     useEffect(() => {
@@ -19,10 +21,63 @@ const Spell = () => {
 
     return (
         <div>
-            <h1>Hello</h1>
-            <h3>{details.range}</h3>
+            <h3>{details.name}</h3>
+            <DetailWrapper>
+                <Info>
+                    <p>{details.desc}</p>
+                    {/* <p>{details.subclasses.name}</p> */}
+                    {/* <p>{details.school.name}</p> */}
+                </Info>
+            </DetailWrapper>
         </div>
     )
 }
+
+const DetailWrapper = styled.div`
+    margin-top: 2rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+    grid-gap: 3rem;
+
+    .active {
+        background: linear-gradient(35deg, #494949, #313131);
+        color: white;
+    }
+`
+const Button = styled.button`
+    margin-right: 2rem;
+    border: 2px solid black;
+    color: #313131;
+    background-color: white;
+    padding: 1rem 2rem;
+    font-weight: 700;
+    border-radius: 1rem;
+`
+
+const Image = styled.img`
+    max-width: 100%;
+    border-radius: 2rem;
+`
+
+const Info = styled.div`
+    font-weight: 400;
+
+    p {
+        margin: 2rem 0rem;
+    }
+
+    ul {
+        margin: 2rem 0rem;
+    }
+
+    li {
+        margin-bottom: 0.5rem;
+    }
+
+    a {
+        color: black;
+        text-decoration: none;
+    }
+`
 
 export default Spell
