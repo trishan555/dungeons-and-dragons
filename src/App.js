3fd3ca6a7af7f5376bf4a2d7001faf5b53124666
TrishLeaf <trishanp96@gmail.com>
2022-09-16 19:09:57 +0530

    basics route added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Spells from './Components/Spells'
+import Spell from './Components/Spells'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Spells />} />
+                <Route path='/spell/:name' element={<Spell />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
